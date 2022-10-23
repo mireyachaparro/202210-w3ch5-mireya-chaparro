27adfa9945bcc1ee10e5125ec68630f6b5886d1f
mireyachaparro <mireyacn99@gmail.com>
2022-10-23 17:00:18 +0200

    Add previous button

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import { Buttons } from './components/buttons.js';
 
+// import { Details } from './components/delails.js';
 import { Header } from './components/header.js';
 import { Main } from './components/main.js';
 
@@ -7,5 +8,6 @@ console.log('loadead index');
 
 new Header('header');
 new Main('slot');
+// new Details('main');
 //new Buttons('.buttons');
 //
